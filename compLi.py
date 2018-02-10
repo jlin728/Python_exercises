6a4ec52e80e894c9d069f7485e487a695c4d7670
bonjourjiff <35427835+jlin728@users.noreply.github.com>
2018-02-10 11:44:58 -0500

    Add files via upload

---
+++ compLi.py
@@ -0,0 +1,23 @@
+'''
+list_one = [1,2,5,6,2]
+list_two = [1,2,5,6,2]
+
+
+list_one = [1,2,5,6,5]
+list_two = [1,2,5,6,5,3]
+
+
+list_one = [1,2,5,6,5,16]
+list_two = [1,2,5,6,5]
+'''
+
+list_one = ['celery','carrots','bread','milk']
+list_two = ['celery','carrots','bread','cream']
+
+
+if list_one == list_two:
+    print "These lists are the same =)"
+else:
+    print "NOT THE SAME"
+
+ 
---
+++ dictBas.py
@@ -0,0 +1,11 @@
+users = {
+    "name":"Anna",
+    "age":101,
+    "birthCountry":"USA",
+    "favLang":"Python"
+}
+
+def anna():
+    print "My name is", users["name"], "\nMy age is", users["age"], "\nMy country of birth is", users["birthCountry"], "\nMy favorite language is", users["favLang"]
+
+anna()
